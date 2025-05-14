efbb4939ac90bccca5bf31f55b02161bc5f6cc93
soulvibescorp <113645673+soulvibescorp@users.noreply.github.com>
2025-05-14 16:51:44 -0400

    Create data.js

---
+++ tools/free-movies/data.js
@@ -0,0 +1,15 @@
+const API_KEYS = {
+    youtube: 'YOUR_YOUTUBE_API_KEY',
+    tmdb: 'YOUR_TMDB_API_KEY',
+    vimeo: 'YOUR_VIMEO_API_KEY',
+    pexels: 'YOUR_PEXELS_API_KEY',
+    dailymotion: 'YOUR_DAILYMOTION_API_KEY'
+};
+
+const API_URLS = {
+    youtube: `https://www.googleapis.com/youtube/v3/search`,
+    tmdb: `https://api.themoviedb.org/3/movie/popular`,
+    vimeo: `https://api.vimeo.com/videos`,
+    pexels: `https://api.pexels.com/videos/search`,
+    dailymotion: `https://api.dailymotion.com/videos`
+};
