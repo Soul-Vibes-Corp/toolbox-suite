084ff7e4c1cd29684beb2eb592f2788630dfc3aa
soulvibescorp <113645673+soulvibescorp@users.noreply.github.com>
2025-05-14 17:07:58 -0400

    Update data.js

--- tools/free-movies/data.js
+++ tools/free-movies/data.js
@@ -1,3 +1,5 @@
+// data.js - API Configuration and Endpoints
+
 const API_KEYS = {
     youtube: 'YOUR_YOUTUBE_API_KEY',
     tmdb: 'YOUR_TMDB_API_KEY',
@@ -7,9 +9,15 @@ const API_KEYS = {
 };
 
 const API_URLS = {
-    youtube: `https://www.googleapis.com/youtube/v3/search`,
-    tmdb: `https://api.themoviedb.org/3/movie/popular`,
+    youtube: `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&key=${API_KEYS.youtube}&q=`,
+    tmdb: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEYS.tmdb}&language=en-US&page=1`,
     vimeo: `https://api.vimeo.com/videos`,
     pexels: `https://api.pexels.com/videos/search`,
-    dailymotion: `https://api.dailymotion.com/videos`
+    dailymotion: `https://api.dailymotion.com/videos`,
+    archive: `https://archive.org/advancedsearch.php?q=subject:movie&output=json&rows=10`
+};
+
+const HEADERS = {
+    pexels: { 'Authorization': API_KEYS.pexels },
+    vimeo: { 'Authorization': `Bearer ${API_KEYS.vimeo}` }
 };
