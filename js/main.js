374f3f83736283a1dc66697aaca097ba9349c149
soulvibescorp <113645673+soulvibescorp@users.noreply.github.com>
2025-05-19 20:53:00 -0400

    Update and rename main.js to js/main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ auth.onAuthStateChanged(user => {
   if (user) {
     db.collection("users").doc(user.uid).get().then(doc => {
       const data = doc.data();
-      document.getElementById("pointsDisplay").innerText = data.points;
-      // populate rewards list, tool usage, etc.
+      document.getElementById("pointsDisplay").innerText = data.points || 0;
     });
   } else {
     window.location.href = "login.html";
